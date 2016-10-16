4cd44aa0040062e2fd3d4415c3554251dc6dd67a
jhangakash <jhangakash@outlook.com>
2016-10-16 18:27:10 +0530

    27th commit

---
+++ app/src/main/java/envago/envago/AdventureForm.java
@@ -0,0 +1,17 @@
+package envago.envago;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jhang on 10/16/2016.
+ */
+public class AdventureForm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.create_adventure_form);
+    }
+}
