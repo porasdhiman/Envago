56c4ad233dc18c62593ec73494e840251ab59042
jhangakash <jhangakash@outlook.com>
2016-10-17 14:04:06 +0530

    29th commit

---
+++ app/src/main/java/envago/envago/CallVolunteerForm.java
@@ -0,0 +1,18 @@
+package envago.envago;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jhang on 10/17/2016.
+ */
+public class CallVolunteerForm extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.call_volunteers_form);
+    }
+}
