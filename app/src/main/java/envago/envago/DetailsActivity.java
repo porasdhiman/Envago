6f9c99539599c57bfea7908e6259be3d18368075
jhangakash <jhanagakash@outlook.com>
2016-10-24 18:05:52 +0530

    45th Commit

--- app/src/main/java/envago/envago/DetailsActivity.java
+++ app/src/main/java/envago/envago/DetailsActivity.java
@@ -1,13 +1,17 @@
 package envago.envago;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -55,12 +59,20 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.paypal.android.sdk.payments.PayPalAuthorization;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -113,6 +125,28 @@ public class DetailsActivity extends FragmentActivity implements View.OnClickLis
     private Location mLastLocation;
     //--------------Google search api variable------------
     protected GoogleApiClient mGoogleApiClient;
+
+    //-----------------------------------Paypal variable
+
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
+    private static final String CONFIG_CLIENT_ID = "Af64A4qjlISjmdbpxvgMs47i5Yl_Quuh8u4Ah-tfxo_DRH2ya-9MQpIEY2XLElcraeq2ox8Xvx6kUJRx";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+            // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Hipster Store")
+            .merchantPrivacyPolicyUri(
+                    Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(
+                    Uri.parse("https://www.example.com/legal"));
+
+    PayPalPayment thingToBuy;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -187,6 +221,10 @@ public class DetailsActivity extends FragmentActivity implements View.OnClickLis
         markers = new Hashtable<String, String>();
         mapFragment.getMapAsync(this);
 
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+
     }
 
     private void setUiPageViewController() {
@@ -254,6 +292,14 @@ public class DetailsActivity extends FragmentActivity implements View.OnClickLis
                 route_txtView.setTextColor(getResources().getColor(R.color.White));
                 about_txtView.setTextColor(getResources().getColor(R.color.White));
                 break;
+            case R.id.purchase_btn:
+                thingToBuy = new PayPalPayment(new BigDecimal("10"), "USD",
+                        "HeadSet", PayPalPayment.PAYMENT_INTENT_SALE);
+                Intent intent = new Intent(DetailsActivity.this, PaymentActivity.class);
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+                startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+                break;
+
         }
 
     }
@@ -835,6 +881,96 @@ public class DetailsActivity extends FragmentActivity implements View.OnClickLis
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+    }
+
+    //---------------------------------------------Payment method----------------------------------
+    public void onFuturePaymentPressed(View pressed) {
+        Intent intent = new Intent(DetailsActivity.this,
+                PayPalFuturePaymentActivity.class);
+
+        startActivityForResult(intent, REQUEST_CODE_FUTURE_PAYMENT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirm != null) {
+                    try {
+                        System.out.println(confirm.toJSONObject().toString(4));
+                        System.out.println(confirm.getPayment().toJSONObject().toString(4));
+
+//                        Toast.makeText(getActivity(), "Registered", Toast.LENGTH_LONG).show();
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                System.out.println("The user canceled.");
+            } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+                System.out
+                        .println("An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+            }
+        } else if (requestCode == REQUEST_CODE_FUTURE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                PayPalAuthorization auth = data
+                        .getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+                if (auth != null) {
+                    try {
+                        Log.e("FuturePaymentExample", auth.toJSONObject()
+                                .toString(4));
+
+                        String authorization_code = auth.getAuthorizationCode();
+                        Log.e("FuturePaymentExample", authorization_code);
+
+                        sendAuthorizationToServer(auth);
+                        Toast.makeText(DetailsActivity.this,
+                                "Future Payment code received from PayPal",
+                                Toast.LENGTH_LONG).show();
+
+                    } catch (JSONException e) {
+                        Log.e("FuturePaymentExample",
+                                "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.e("FuturePaymentExample", "The user canceled.");
+            } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.e("FuturePaymentExample",
+                        "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+            }
+        }
+    }
+
+    private void sendAuthorizationToServer(PayPalAuthorization authorization) {
+
+    }
+
+    public void onFuturePaymentPurchasePressed(View pressed) {
+        // Get the Application Correlation ID from the SDK
+        String correlationId = PayPalConfiguration
+                .getApplicationCorrelationId(DetailsActivity.this);
+
+        Log.i("FuturePaymentExample", "Application Correlation ID: "
+                + correlationId);
+
+        // TODO: Send correlationId and transaction details to your server for
+        // processing with
+        // PayPal...
+        Toast.makeText(DetailsActivity.this,
+                "App Correlation ID received from SDK", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public void onDestroy() {
+        // Stop service when done
+        stopService(new Intent(DetailsActivity.this, PayPalService.class));
+        super.onDestroy();
     }
 
 }
